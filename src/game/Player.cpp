14cf2a1db0c232fa2833c72b6b3871e155f536fa
Faq <faq@noname.com>
2010-11-03 22:25:38 +0200

    [10653] Implemented MSG_MOVE_KNOCK_BACK opcode

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -6094,6 +6094,8 @@ bool Player::SetPosition(float x, float y, float z, float orientation, bool tele
         else
             RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_TURNING);
 
+        RemoveSpellsCausingAura(SPELL_AURA_FEIGN_DEATH);
+
         // move and update visible state if need
         m->PlayerRelocation(this, x, y, z, orientation);
 
