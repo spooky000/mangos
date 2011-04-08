5574f7c10f9d3fe1aa5c715d8858cba60884be4c
tibbi <tibbi@centrum.sk>
2011-04-08 23:43:16 +0200

    Adding exception for non targetable seat flag from michalpolko

--- src/game/Vehicle.cpp
+++ src/game/Vehicle.cpp
@@ -169,7 +169,17 @@ bool VehicleKit::AddPassenger(Unit *passenger, int8 seatId)
 
     if (seatInfo->m_flags & SEAT_FLAG_UNATTACKABLE)
     {
-        passenger->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+        // some exceptions where passengets should be targetable, seems that flag is wrong
+        switch (m_pBase->GetEntry())
+        {
+            case 33118:                  // Ignis slag pot
+            case 32934:                  // Kologarn Right Arm
+                break;
+            default:
+                passenger->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+                break;
+        }
+        passenger->RemoveSpellsCausingAura(SPELL_AURA_MOD_SHAPESHIFT);
     }
 
     if (seatInfo->m_flags & SEAT_FLAG_CAN_CONTROL)
