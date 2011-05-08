13328e39c771c08ac78e63d27780c0374338bdb8
tibbi <tibbi@centrum.sk>
2011-05-08 23:55:33 +0200

    compile fix

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6509,7 +6509,7 @@ void Aura::HandleAuraModIncreaseEnergyPercent(bool apply, bool /*Real*/)
 
     UnitMods unitMod = UnitMods(UNIT_MOD_POWER_START + powerType);
 
-    target()->HandleStatModifier(unitMod, TOTAL_PCT, float(m_modifier.m_amount), apply);
+    target->HandleStatModifier(unitMod, TOTAL_PCT, float(m_modifier.m_amount), apply);
 }
 
 void Aura::HandleAuraModIncreaseHealthPercent(bool apply, bool /*Real*/)
