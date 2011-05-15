18426f4f2096a3518a0414628860feb87c04aa18
tibbi <tibbi@centrum.sk>
2011-05-15 20:01:47 +0200

    reverting "moving Fingers of Frost stackamount handling"

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2467,6 +2467,13 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                 }
                 break;
             }
+            case SPELLFAMILY_MAGE:
+            {
+                // Fingers of Frost stacks set to max at apply
+                if (GetId() == 74396)
+                    GetHolder()->SetAuraCharges(GetSpellProto()->StackAmount);
+                break;
+            }
             case SPELLFAMILY_HUNTER:
             {
                 switch(GetId())
@@ -9351,7 +9358,6 @@ m_permanent(false), m_isRemovedOnShapeLost(true), m_deleted(false), m_in_use(0)
         case 55166:                                         // Tidal Force
         case 58914:                                         // Kill Command (pet part)
         case 71564:                                         // Deadly Precision
-        case 74396:                                         // Fingers of Frost
             m_stackAmount = m_spellProto->StackAmount;
             break;
     }
