86f20d17448e216bc65fddcd5b30d7a220a560d8
Feanordev <Feanordev@gmail.com>
2011-05-16 21:06:16 +0200

    Petrified Bark testcode for Paytheo

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -602,6 +602,14 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura
                     triggered_spell_id = 25997;
                     break;
                 }
+                // Petrified Bark normal and heroic mode
+                case 62337:
+                case 62933:
+                {
+                    basepoints[0] = damage;
+                    triggered_spell_id = 62379;
+                    break;
+                }
                 // Sweeping Strikes (NPC spells may be)
                 case 18765:
                 case 35429:
