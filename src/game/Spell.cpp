f2009c4f5a104343d5b4685f80fda2ebab0f4f1f
Feanordev <Feanordev@gmail.com>
2011-06-27 23:19:20 +0200

    reverted test

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -634,7 +634,6 @@ void Spell::FillTargetMap()
                 }
                 break;
             case TARGET_AREAEFFECT_CUSTOM:
-            case TARGET_DUELVSPLAYER:
             default:
                 switch(m_spellInfo->EffectImplicitTargetB[i])
                 {
