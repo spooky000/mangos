720f9b07be0150e56bb00749453d2f900dee5b42
tibbi <tibbi@centrum.sk>
2011-08-10 16:25:38 +0200

    prevent pet continuing attack after target becomes invisible  by darkstalker

--- src/game/PetAI.cpp
+++ src/game/PetAI.cpp
@@ -59,9 +59,10 @@ void PetAI::MoveInLineOfSight(Unit *u)
         float attackRadius = m_creature->GetAttackDistance(u);
         if(m_creature->IsWithinDistInMap(u, attackRadius) && m_creature->GetDistanceZ(u) <= CREATURE_Z_ATTACK_RANGE)
         {
-            if(m_creature->IsWithinLOSInMap(u))
+            if (!m_creature->hasUnitState(UNIT_STAT_CAN_NOT_REACT) && m_creature->IsWithinLOSInMap(u))
             {
                 AttackStart(u);
+                u->RemoveSpellsCausingAura(SPELL_AURA_MOD_STEALTH);
             }
         }
     }
