05a9bbb398f7f2868364891ef84bc1a91018c75f
Omas Abdullah <omas.abdullah@gmail.com>
2011-08-18 01:30:18 -0400

    return statement missing causing Lightning Arrows on XT002 Gravity bomb

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5758,13 +5758,13 @@ void Aura::HandlePeriodicTriggerSpell(bool apply, bool /*Real*/)
                     if (pCaster->HasAura(GetModifier()->m_amount))
                         pCaster->CastSpell(target, spellId, true);
                 }
+                return;
             case 66083:                                     // Lightning Arrows (Trial of the Champion encounter)
                 if (m_removeMode == AURA_REMOVE_BY_EXPIRE)
                 {
                     if (Unit* pCaster = GetCaster())
                         pCaster->CastSpell(pCaster, 66085, true, NULL, this);
                 }
-
                 return;
             case 71441:                                     // Unstable Ooze Explosion (Icecrown Citadel encounter)
                 if (m_removeMode == AURA_REMOVE_BY_EXPIRE)
