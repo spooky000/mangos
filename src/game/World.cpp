a80f513a54f270cf64e92588d75883eb1356a0ae
/dev/rsa <kris13@bk.ru>
2011-09-07 12:20:24 +0200

    [mr940] really use player EquipGearScore for vehicle GearScale spells. formula may be need correct, but seems offlike.

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -591,6 +591,8 @@ void World::LoadConfigSettings(bool reload)
     setConfig(CONFIG_BOOL_LFR_EXTEND, "LFR.Extend",false);
     setConfigMinMax(CONFIG_UINT32_LFG_MAXKICKS, "LFG.MaxKicks", 5, 1, 10);
 
+    setConfigMinMax(CONFIG_UINT32_GEAR_CALC_BASE, "Player.GSCalculationBase", 190, 1, 384);
+
     setConfigMinMax(CONFIG_UINT32_START_PLAYER_MONEY, "StartPlayerMoney", 0, 0, MAX_MONEY_AMOUNT);
 
     setConfig(CONFIG_UINT32_MAX_HONOR_POINTS, "MaxHonorPoints", 75000);
