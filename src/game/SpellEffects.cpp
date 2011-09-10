a4b14ed384870d81f6125013f42f2e7873c987ce
tibbi <tibbi@centrum.sk>
2011-09-10 14:40:03 +0200

    Blink..

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -10529,7 +10529,7 @@ void Spell::EffectLeapForward(SpellEffectIndex eff_idx)
         float ox, oy, oz;
         unitTarget->GetPosition(ox, oy, oz);
         float fx, fy, fz;
-        fz = oz + 0.05f;
+        fz = oz + 2.f;
         fx = unitTarget->GetPositionX() + distance * cos(unitTarget->GetOrientation());
         fy = unitTarget->GetPositionY() + distance * sin(unitTarget->GetOrientation());
 
