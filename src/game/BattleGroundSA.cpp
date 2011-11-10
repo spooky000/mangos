d8135ac1426eccd505712b1837b5b1949bad2f1b
tibbi <tibbi@centrum.sk>
2011-11-09 18:45:54 -0800

    SotA - another attempt to delete ships well in 2nd round

--- src/game/BattleGroundSA.cpp
+++ src/game/BattleGroundSA.cpp
@@ -1055,12 +1055,14 @@ void BattleGroundSA::SendTransportsRemove(Player * player)
             boat1->BuildOutOfRangeUpdateBlock(&transData);
             boat1->SetRespawnTime(0);
             boat1->Delete();
+            DelObject(BG_SA_BOAT_ONE);
         }
         if (GameObject * boat2 = GetBGObject(BG_SA_BOAT_TWO))
         {
             boat2->BuildOutOfRangeUpdateBlock(&transData);
             boat2->SetRespawnTime(0);
             boat2->Delete();
+            DelObject(BG_SA_BOAT_TWO);
         }
         WorldPacket packet;
         transData.BuildPacket(&packet);
