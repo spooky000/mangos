29a4ec3f3f17cc18328bc96b6a5dd84752753d80
/dev/rsa <kris13@bk.ru>
2011-12-02 12:00:56 +0100

    fix crush in 1341 && typo. Thx to raynar && Zakamurite.

--- src/game/VehicleHandler.cpp
+++ src/game/VehicleHandler.cpp
@@ -202,9 +202,6 @@ void WorldSession::HandleEjectPassenger(WorldPacket &recv_data)
 
     VehicleKit* pVehicle = passenger->GetVehicle();
 
-    if (!pVehicle)
-        return;
-
     if (!pVehicle ||
        ((pVehicle != GetPlayer()->GetVehicleKit()) &&
         !(pVehicle->GetBase()->GetVehicleInfo()->GetEntry()->m_flags & (VEHICLE_FLAG_ACCESSORY))))
