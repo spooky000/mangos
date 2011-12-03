3084a8fbc6083837cec12765c4fed25270323a00
tibbi <tibbi@centrum.sk>
2011-12-03 14:28:52 +0100

    Removing a weird hack from sota brackets...

--- src/game/BattleGroundMgr.cpp
+++ src/game/BattleGroundMgr.cpp
@@ -1608,8 +1608,7 @@ BattleGround * BattleGroundMgr::CreateNewBattleGround(BattleGroundTypeId bgTypeI
     }
 
     // set before Map creating for let use proper difficulty
-    if (!(bg->GetMapId() == 607))
-        bg->SetBracket(bracketEntry);
+    bg->SetBracket(bracketEntry);
 
     // will also set m_bgMap, instanceid
     sMapMgr.CreateBgMap(bg->GetMapId(), bg);
@@ -1621,8 +1620,6 @@ BattleGround * BattleGroundMgr::CreateNewBattleGround(BattleGroundTypeId bgTypeI
 
     // start the joining of the bg
     bg->SetStatus(STATUS_WAIT_JOIN);
-    if (bg->GetMapId() == 607)
-        bg->SetBracket(bracketEntry);
     bg->SetArenaType(arenaType);
     bg->SetRated(isRated);
     bg->SetRandom(isRandom);
