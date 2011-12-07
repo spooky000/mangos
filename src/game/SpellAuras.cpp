35ac48c8ac9210a947641044164694fd2f6364b9
/dev/rsa <kris13@bk.ru>
2011-12-07 22:34:30 +0100

    implement new things: - heartbert resist; - DOT by immune immediate break and logging; - DOT damage immune logging. TESTING

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -648,6 +648,16 @@ void Aura::Update(uint32 diff)
             PeriodicTick();
         }
     }
+    else
+    {
+        if (m_periodicTimer <= 0)
+        {
+            m_periodicTimer = SPELL_SPELL_CHANNEL_UPDATE_INTERVAL;
+            PeriodicCheck();
+        }
+        else
+            m_periodicTimer -= diff;
+    }
 }
 
 void Aura::AreaAuraUpdate(uint32 diff)
@@ -8118,6 +8128,13 @@ void Aura::PeriodicTick()
     if (!target || !spellProto)
         return;
 
+    if (target->IsImmuneToSpell(spellProto))
+    {
+        if (Unit* caster = GetCaster())
+            caster->SendSpellDamageImmune(target, spellProto->Id);
+        return;
+    }
+
     switch(m_modifier.m_auraname)
     {
         case SPELL_AURA_PERIODIC_DAMAGE:
@@ -8139,8 +8156,11 @@ void Aura::PeriodicTick()
                 return;
 
             // Check for immune (not use charges)
-            if(target->IsImmunedToDamage(GetSpellSchoolMask(spellProto)))
+            if (target->IsImmunedToDamage(GetSpellSchoolMask(spellProto)))
+            {
+                pCaster->SendSpellDamageImmune(target, spellProto->Id);
                 return;
+            }
 
             // some auras remove at specific health level or more
             if(m_modifier.m_auraname == SPELL_AURA_PERIODIC_DAMAGE)
@@ -9535,6 +9555,52 @@ void Aura::PeriodicDummyTick()
                 uint32 deal = m_modifier.m_amount * target->GetMaxHealth() / 100;
                 target->DealDamage(target, deal, NULL, NODAMAGE, SPELL_SCHOOL_MASK_NORMAL, NULL, false);
                 return;
+            }
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+void Aura::PeriodicCheck()
+{
+
+    SpellAuraHolderPtr holder = GetHolder();
+    SpellEntry const* spellProto = GetSpellProto();
+
+    if (!holder || !spellProto)
+        return;
+
+    Unit* target = GetTarget();
+    Unit* caster = GetCaster();
+
+    if (!caster || !target)
+        return;
+
+    if (target->IsImmuneToSpell(spellProto))
+    {
+        if (Unit* caster = GetCaster())
+            caster->SendSpellDamageImmune(target, spellProto->Id);
+        target->RemoveAurasDueToSpell(GetId());
+        return;
+    }
+
+    switch(m_modifier.m_auraname)
+    {
+        case SPELL_AURA_MOD_CONFUSE:
+        case SPELL_AURA_MOD_FEAR:
+        case SPELL_AURA_MOD_STUN:
+        case SPELL_AURA_MOD_ROOT:
+        case SPELL_AURA_TRANSFORM:
+        {
+            if (caster->GetObjectGuid().IsPlayer() && target->GetObjectGuid().IsCreatureOrVehicle())
+            {
+                if (caster->MagicSpellHitResult(target, spellProto) != SPELL_MISS_NONE)
+                {
+                    caster->SendSpellDamageResist(target, spellProto->Id);
+                    target->RemoveAurasDueToSpell(GetId());
+                }
             }
             break;
         }
