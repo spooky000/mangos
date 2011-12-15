ca178755e7fdbbf06d6d47f045a0a0a39a450a91
tibbi <tibbi@centrum.sk>
2011-12-15 22:44:19 +0100

    adding a check to SpellAuraHolder::HandleSpellSpecificBoostsForward

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -11593,6 +11593,9 @@ void SpellAuraHolder::HandleSpellSpecificBoostsForward(bool apply)
     if (!pCaster)
         pCaster = m_target;
 
+    if (!pCaster)
+        return;
+
     uint32 procFlag = apply ? PROC_FLAG_ON_AURA_APPLY : PROC_FLAG_ON_AURA_FADE;
     uint32 procEx   = 0;
     switch (m_removeMode)
