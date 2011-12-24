12547b2cecb02a56d4d540593d1ce7f4a8634fc3
Reamer <Philipp@.(none)>
2011-12-24 20:22:17 +0100

    Rewrite Mark of the Faceless

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -8607,6 +8607,20 @@ bool Spell::FillCustomTargetMap(SpellEffectIndex i, UnitList &targetUnitMap)
         //    FillAreaTargets(targetUnitMap, radius, PUSH_DEST_CENTER, SPELL_TARGETS_AOE_DAMAGE);
         //    targetUnitMap.remove(m_caster); // exclude caster
         //}
+        case 63278: // Mark of Faceless
+        {
+            if (i != EFFECT_INDEX_1)
+                return false;
+
+            Unit* currentTarget = m_targets.getUnitTarget();
+            if (currentTarget)
+            {
+                m_targets.setDestination(currentTarget->GetPositionX(), currentTarget->GetPositionY(), currentTarget->GetPositionZ());
+                FillAreaTargets(targetUnitMap, radius, PUSH_DEST_CENTER, SPELL_TARGETS_AOE_DAMAGE);
+                targetUnitMap.remove(currentTarget);
+            }
+            break;
+        }
         case 65044: // Flames
         case 65045: // Flame of demolisher
         {
