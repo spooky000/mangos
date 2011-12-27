283fdda33c3b64f0efb8df97663e6b6c3bfae6db
tibbi <tibbi@centrum.sk>
2011-12-27 23:03:14 +0100

    small hitbox change for pve

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11975,9 +11975,7 @@ void Unit::UpdateModelData()
         else
         {
             // We expect values in database to be relative to scale = 1.0
-            float scaled_radius = GetObjectScale() * modelInfo->bounding_radius;
-
-            boundingRadius = scaled_radius < 2.0f ? scaled_radius : 2.0f;
+            boundingRadius = modelInfo->combat_reach;
             combatReach = GetObjectScale() * (modelInfo->bounding_radius < 2.0 ? modelInfo->combat_reach : modelInfo->combat_reach / modelInfo->bounding_radius);
         }
     }
