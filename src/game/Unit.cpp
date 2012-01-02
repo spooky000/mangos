c72161cb2699e76320d1a499b32bc67f0e318fd0
tibbi <tibbi@centrum.sk>
2012-01-02 14:53:34 +0100

    modifying arc of mobs in front of the tank

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -604,10 +604,8 @@ void Unit::GetRandomContactPoint(const Unit* obj, float &x, float &y, float &z, 
     if (attacker_number > 0)
         --attacker_number;
 
-    float angle = ((attacker_number) ? float(rand_norm() - 0.5f) : 0);
+    float angle = ((attacker_number) ? (frand(-0.7f, 0.7f)) : 0);
 
-    //GetNearPoint(obj, x, y, z, obj->GetCombatReach(), distance2dMin+(distance2dMax-distance2dMin) * (float)rand_norm()
-      //  , GetAngle(obj) + (attacker_number ? (static_cast<float>(M_PI/2) - static_cast<float>(M_PI) * (float)rand_norm()) * float(attacker_number) / GetCombatReach() * 0.3f : 0));
     GetNearPoint(obj, x, y, z, obj->GetCombatReach(), distance2dMin + (distance2dMax - distance2dMin) * (float)rand_norm()
         , GetAngle(obj) + angle);
 }
