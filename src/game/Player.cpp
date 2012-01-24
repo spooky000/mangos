ba65631afff37d1f1cc4f1472ba592173024ac2c
/dev/rsa <kris13@bk.ru>
2012-01-24 20:11:24 +0100

    tempfix in SendWorldState

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -8600,7 +8600,10 @@ void Player::SendUpdateWorldState(uint32 Field, uint32 Value)
     WorldPacket data(SMSG_UPDATE_WORLD_STATE, 8);
     data << Field;
     data << Value;
-    GetSession()->SendPacket(&data);
+
+    // Tempfix before WorldStateMgr implementing
+    if (IsInWorld())
+        GetSession()->SendPacket(&data);
 }
 
 static WorldStatePair AV_world_states[] =
