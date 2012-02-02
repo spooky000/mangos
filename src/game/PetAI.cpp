c23707a89d2bceacff8e4f579b31fe10d74286ab
tibbi <tibbi@centrum.sk>
2012-02-02 18:36:39 +0100

    stop pet attacking just at Confused moving

--- src/game/PetAI.cpp
+++ src/game/PetAI.cpp
@@ -157,7 +157,8 @@ void PetAI::UpdateAI(const uint32 diff)
             _stopAttack();
             return;
         }
-        else if (!m_creature->getVictim()->isAlive())        // Stop attack if target dead
+        else if (!m_creature->getVictim()->isAlive() ||
+                  m_creature->getVictim()->hasUnitState(UNIT_STAT_CONFUSED_MOVE))        // Stop attack if target dead or confused
         {
             m_creature->InterruptNonMeleeSpells(false);
             _stopAttack();
