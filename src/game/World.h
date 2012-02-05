e113ca3a358c0fabce8cdf9aa5c7c353b6dd2b2e
Feanordev <Feanordev@gmail.com>
2012-02-05 23:53:51 +0100

    updatediff @ server info

--- src/game/World.h
+++ src/game/World.h
@@ -523,6 +523,7 @@ class World
         time_t const& GetGameTime() const { return m_gameTime; }
         /// Uptime (in secs)
         uint32 GetUptime() const { return uint32(m_gameTime - m_startTime); }
+        uint32 getUpdateTime() const { return m_updateTime; }
         /// Next daily quests reset time
         time_t GetNextDailyQuestsResetTime() const { return m_NextDailyQuestReset; }
         time_t GetNextWeeklyQuestsResetTime() const { return m_NextWeeklyQuestReset; }
@@ -656,6 +657,7 @@ class World
 
         time_t m_startTime;
         time_t m_gameTime;
+        uint32 m_updateTime;
         IntervalTimer m_timers[WUPDATE_COUNT];
         uint32 mail_timer;
         uint32 mail_timer_expires;
