7bc2693d28241198014ef1b4cdf83d6ecb522f2d
tibbi <tibbi@centrum.sk>
2012-02-07 16:02:18 +0100

    [11907] Add files for MMaps Implementation

--- src/game/MoveMapSharedDefines.h
+++ src/game/MoveMapSharedDefines.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -50,5 +50,3 @@ enum NavTerrain
     NAV_UNUSED4 = 0x80
     // we only have 8 bits
 };
-
-#endif  // _MOVE_MAP_SHARED_DEFINES_H
