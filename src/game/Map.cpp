4bb3f81823de4488c732c64a1cf27259a7ef028c
Feanordev <Feanordev@gmail.com>
2012-02-09 20:36:14 +0100

    slight code cleanup

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -450,6 +450,34 @@ bool Map::loaded(const GridPair &p) const
     return ( getNGrid(p.x_coord, p.y_coord) && isGridObjectDataLoaded(p.x_coord, p.y_coord) );
 }
 
+void Map::VisitNearbyCellsOf(WorldObject* obj, TypeContainerVisitor<MaNGOS::ObjectUpdater, GridTypeMapContainer> &gridVisitor, TypeContainerVisitor<MaNGOS::ObjectUpdater, WorldTypeMapContainer> &worldVisitor)
+{
+    // Check for valid position
+    if (!obj->IsPositionValid())
+        return;
+
+    // Update mobs/objects in ALL visible cells around object!
+    CellArea area = Cell::CalculateCellArea(obj->GetPositionX(), obj->GetPositionY(), GetVisibilityDistance());
+
+    for (uint32 x = area.low_bound.x_coord; x <= area.high_bound.x_coord; ++x)
+    {
+        for (uint32 y = area.low_bound.y_coord; y <= area.high_bound.y_coord; ++y)
+        {
+            // marked cells are those that have been visited
+            // don't visit the same cell twice
+            uint32 cell_id = (y * TOTAL_NUMBER_OF_CELLS_PER_MAP) + x;
+            if (isCellMarked(cell_id))
+                continue;
+
+            markCell(cell_id);
+            CellPair pair(x, y);
+            Cell cell(pair);
+            Visit(cell, gridVisitor);
+            Visit(cell, worldVisitor);
+        }
+    }
+}
+
 void Map::Update(const uint32 &t_diff)
 {
     /// update worldsessions for existing players
@@ -483,6 +511,7 @@ void Map::Update(const uint32 &t_diff)
         if (!plr || !plr->IsInWorld())
             continue;
 
+        /// update players at tick
         WorldObject::UpdateHelper helper(plr);
         helper.Update(t_diff);
 
@@ -490,26 +519,7 @@ void Map::Update(const uint32 &t_diff)
             continue;
 
         //lets update mobs/objects in ALL visible cells around player!
-        CellArea area = Cell::CalculateCellArea(plr->GetPositionX(), plr->GetPositionY(), GetVisibilityDistance());
-
-        for(uint32 x = area.low_bound.x_coord; x <= area.high_bound.x_coord; ++x)
-        {
-            for(uint32 y = area.low_bound.y_coord; y <= area.high_bound.y_coord; ++y)
-            {
-                // marked cells are those that have been visited
-                // don't visit the same cell twice
-                uint32 cell_id = (y * TOTAL_NUMBER_OF_CELLS_PER_MAP) + x;
-                if(!isCellMarked(cell_id))
-                {
-                    markCell(cell_id);
-                    CellPair pair(x,y);
-                    Cell cell(pair);
-                    cell.SetNoCreate();
-                    Visit(cell, grid_object_update);
-                    Visit(cell, world_object_update);
-                }
-            }
-        }
+        VisitNearbyCellsOf(plr, grid_object_update, world_object_update);
     }
 
     // non-player active objects
@@ -527,27 +537,7 @@ void Map::Update(const uint32 &t_diff)
             if (!obj->IsInWorld() || !obj->IsPositionValid())
                 continue;
 
-            //lets update mobs/objects in ALL visible cells around player!
-            CellArea area = Cell::CalculateCellArea(obj->GetPositionX(), obj->GetPositionY(), GetVisibilityDistance());
-
-            for(uint32 x = area.low_bound.x_coord; x <= area.high_bound.x_coord; ++x)
-            {
-                for(uint32 y = area.low_bound.y_coord; y <= area.high_bound.y_coord; ++y)
-                {
-                    // marked cells are those that have been visited
-                    // don't visit the same cell twice
-                    uint32 cell_id = (y * TOTAL_NUMBER_OF_CELLS_PER_MAP) + x;
-                    if(!isCellMarked(cell_id))
-                    {
-                        markCell(cell_id);
-                        CellPair pair(x,y);
-                        Cell cell(pair);
-                        cell.SetNoCreate();
-                        Visit(cell, grid_object_update);
-                        Visit(cell, world_object_update);
-                    }
-                }
-            }
+            VisitNearbyCellsOf(obj, grid_object_update, world_object_update);
         }
     }
 
